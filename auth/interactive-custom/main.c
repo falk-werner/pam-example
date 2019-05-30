db0734b4526c0eddb523c000e2e9a2748043a4aa
Falk Werner <falk.werner@gmx.net>
2019-05-30 10:40:01 +0200

    enhanced custom conversation

--- auth/interactive-custom/main.c
+++ auth/interactive-custom/main.c
@@ -1,10 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+#include <unistd.h>
+#include <getopt.h>
+#include <termios.h>
 
 #include <security/pam_appl.h>
 
 #define MAX_STRING_LENGTH 80
+
+#define ON (true)
+#define OFF (false)
+
+static char * read_line(void)
+{
+    char * result = NULL;
+
+    char buffer[MAX_STRING_LENGTH];
+    char * line = fgets(buffer, MAX_STRING_LENGTH, stdin);
+    if (line == buffer)
+    {
+        size_t length = strlen(line);
+        if ((0 < length) && (line[length - 1] == '\n'))
+        {
+            line[length - 1] = '\0';
+        }
+
+        result = strdup(line);
+    }
+
+    return result;
+}
+
+static bool set_echo(bool value)
+{
+    bool old_value = ON;
+
+    struct termios attributes;
+    struct termios new_attributes;
+    int rc = tcgetattr(STDIN_FILENO, &attributes);
+    if (0 == rc) 
+    {
+        new_attributes = attributes;
+        old_value = (0 != (new_attributes.c_lflag & ECHO));
+
+        if (ON == value)
+        {
+            new_attributes.c_lflag |= ECHO;
+        }
+        else
+        {
+            new_attributes.c_lflag &= ~ECHO;
+        }
+        
+        tcsetattr(STDIN_FILENO, TCSANOW, &new_attributes);
+    }
+
+    return old_value;
+}
+
+static char * read_secret()
+{
+    bool echo_mode = set_echo(OFF);
+    char * result = read_line();
+    puts("");
+    set_echo(echo_mode);
+
+    return result;
+}
 
 static int custom_conversation(
     int count,
@@ -17,16 +82,32 @@ static int custom_conversation(
 
     for (int i = 0; i < count; i++)
     {
-        struct pam_response * response = responses[i];   
-        char const * message = messages[i]->msg;
-        char password[MAX_STRING_LENGTH];
-
-        printf("pam: %s ", message);
-        scanf("%s", password);
-
+        struct pam_response * response = responses[i];           
         response->resp_retcode = 0;
-        response->resp = malloc(strlen(password) + 1);
-        strcpy(response->resp, password);        
+        response->resp = NULL;
+
+        int message_type = messages[i]->msg_style;
+        char const * message = messages[i]->msg;
+        switch (message_type)
+        {
+            case PAM_PROMPT_ECHO_OFF:
+                printf("%s", message);
+                response->resp = read_secret(); 
+                break;
+            case PAM_PROMPT_ECHO_ON:
+                printf("%s", message);
+                response->resp = read_line(); 
+                break;
+            case PAM_ERROR_MSG:
+                fprintf(stderr, "error: %s\n ", message);
+                break;
+            case PAM_TEXT_INFO:
+                printf("info: %s\n", message);
+                break;
+            default:
+                fprintf(stderr, "error: unknown pam message type (%d)\n", message_type);
+                break;
+        }
     }
 
     return PAM_SUCCESS;
@@ -35,7 +116,6 @@ static int custom_conversation(
 int main(int argc, char * argv[])
 {
     int rc;
-    char service_name[MAX_STRING_LENGTH];   
     pam_handle_t * handle = NULL;
     struct pam_conv conversation =
     {
@@ -44,7 +124,7 @@ int main(int argc, char * argv[])
     };
 
     printf("authentication service: ");
-    scanf("%s", service_name);
+    char * service_name = read_line();
 
     rc = pam_start(service_name, NULL, &conversation, &handle);
     if (PAM_SUCCESS != rc)
@@ -70,7 +150,7 @@ int main(int argc, char * argv[])
         return EXIT_FAILURE;
     }
 
-    printf("SUCCESS");
+    printf("SUCCESS\n");
 
     pam_end(handle, 0);
     return EXIT_SUCCESS;
